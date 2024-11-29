e449371ecb87128d8abc24ad227aa8fdf1d69473
CJones001 <147557670+CJones001@users.noreply.github.com>
2024-11-29 16:30:59 -0700

    Logging off

--- js/main.js
+++ js/main.js
@@ -2,3 +2,35 @@ import ToDoItem from "./todoitem";
 import ToDoList from "./todolist";
 
 const toDoList = new ToDoList(); 
+
+// Launch App
+document.addEventListener("readystatechange", (event) => 
+{
+    if(event.target.readyState === "complete") 
+    {
+        initApp();
+    }
+});
+
+const initApp = () => 
+{
+    // Add listeners
+
+    // Procedural
+    // load list objects
+    refreshThePage();
+}
+
+const refreshThePage = () => 
+{
+    clearListDisplay();
+    // renderList();
+    // clearItemEntryField();
+    // setFocusOnItemEntry();
+}
+
+const clearListDisplay = () =>
+{
+    const parentElement = document.getElementById("listItems");
+    deleteContents(parentElement);
+}
